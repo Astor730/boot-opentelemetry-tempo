8369d2a4e63073dbf007e7e405cc06ce4e1cf990
Astor730 <mister.p730@gmail.com>
2022-09-06 13:29:14 -0400

    undo

--- boot-otel-tempo-provider1/src/main/java/io/opentelemetry/example/flight/FlightService.java
+++ boot-otel-tempo-provider1/src/main/java/io/opentelemetry/example/flight/FlightService.java
@@ -30,7 +30,6 @@ public class FlightService {
 	@WithSpan
     public void doSomeWorkNewSpan() {
 		LOGGER.info("Doing some work In New span");
-		LOGGER.info("Secondary Work in span");
         Span span = Span.current();
  
         span.setAttribute("template.a2", "some value");
