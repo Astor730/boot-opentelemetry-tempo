5a5d0c2706a96be6045a81f6c06be8dca2b87bac
Astor730 <mister.p730@gmail.com>
2022-09-06 12:04:35 -0400

    added a message

--- boot-otel-tempo-provider1/src/main/java/io/opentelemetry/example/flight/FlightService.java
+++ boot-otel-tempo-provider1/src/main/java/io/opentelemetry/example/flight/FlightService.java
@@ -31,6 +31,7 @@ public class FlightService {
     public void doSomeWorkNewSpan() {
 		LOGGER.info("Doing some work In New span");
         Span span = Span.current();
+		LOGGER.info("Post span creation");
  
         span.setAttribute("template.a2", "some value");
  
